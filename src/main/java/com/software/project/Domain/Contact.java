abd5e37d24bdbc84401504b84b0325131cb201c8
Getnet <getnetadugna25@gmail.com>
2022-03-04 23:02:29 -0500

    Adding the Final Domain  modules

--- src/main/java/com/software/project/Domain/Contact.java
+++ src/main/java/com/software/project/Domain/Contact.java
@@ -6,16 +6,49 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 import lombok.Data;
-@Data
+
 @Entity
 
 public class Contact {
     
+    
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private int id;// by using region number
-    private String Name;//the name of the culture
-    private String Data;
+    private int id;
+    private int userId;
+    private String Name;
+    private String Message;
+    private String Email;
+    public int getId() {
+        return id;
+    }
+    public String getEmail(){
+        return Email;
+    }
+    public void setEmail(String Email){
+        this.Email=Email;
+    }
+   
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return Name;
+    }
+    public void setName(String name) {
+        Name = name;
+    }
+    public String getMessage() {
+        return Message;
+    }
+    public void setMessage(String message) {
+        Message = message;
+    }
 
     
 }
