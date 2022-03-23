bb6d3fb81caca2e8a76c6b6357a5624adecc359c
root <root@Me.me>
2022-03-22 23:06:29 -0400

    Adding the re-ordering and editing the necessary files

--- src/main/java/com/software/project/Repository/ContactRepository.java
+++
@@ -1,8 +0,0 @@
-package com.software.project.Repository;
-import com.software.project.Domain.Contact;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact,Integer>{
-    
-}
--- src/main/java/com/software/project/Repository/CultureRepository.java
+++
@@ -1,8 +0,0 @@
-package com.software.project.Repository;
-import com.software.project.Domain.Culture;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CultureRepository  extends JpaRepository<Culture,Integer>{
-    
-}
